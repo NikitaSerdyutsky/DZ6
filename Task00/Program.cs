6d91099e7da3df52beaa4e7bf75e1e4d0cfc176f
Nikita <serduckijnikita97@gmail.com>
2023-09-26 00:49:06 +0300

    ignore

---
+++ Task00/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите чила М");
+string[] M = Console.ReadLine()!.Split(' ');
+
+int count = 0;
+for (int i = 0; i < M.Length; i++)
+    if (int.Parse(M[i]) > 0)
+        count++;
+
+System.Console.WriteLine(count);
